14d3a45b0f201a5af10bddceb209a26d174a593c
nxcoo7 <92644074+nxcoo7@users.noreply.github.com>
2021-10-16 13:05:12 -0400

    inicio pe
    pagina de putas

---
+++ carguitafeliz.js
@@ -0,0 +1,10 @@
+alert("bienvenida a la pagina de putas de nico");
+
+let antiguedad = prompt("escriba el numero de puta que es de nico");
+
+
+if(antiguedad == 2 ){
+   alert("hola vale chupame el pito:(");
+}else{
+    alert("incorrecto, recuerda que numero de puta eres");
+}
